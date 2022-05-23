14f6d9b6c46b8bdc6db78ea0fa621ed8aab34260
Grolash <Simon.MICHEL@student.umons.ac.be>
2022-05-23 13:02:44 +0100

    first generator, based on weyl middle square sequence

---
+++ weyl_generator.py
@@ -0,0 +1,21 @@
+import datetime
+
+from pi import get_digits
+
+
+class WeylGenerator:
+    def __init__(self):
+        self.seed = int('Ob1' + bin(datetime.datetime.now().microsecond % 100000) + '1')
+        # seed must begin with 1 and end with 1 (ex: 100000000001)
+        self.x = 0
+        self.w = 0
+
+    def next(self):
+        self.x = self.x * self.x % 1000000
+        self.w += self.seed % 1000000
+        self.x += self.w % 1000000
+        self.x = int(str(self.x)[len(self.x)/2:] + str(self.x)[:len(self.x)/2])
+        random = ''
+        for i in range(6):
+            random += get_digits()[i]
+        return int(random)
