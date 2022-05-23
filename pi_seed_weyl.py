34d2b51306ac6d73578ad8e0c1aa2d6e23784946
Grolash <Simon.MICHEL@student.umons.ac.be>
2022-05-23 15:44:17 +0100

    added pi-seeded weyl sequence generator to please Maksym

---
+++ pi_seed_weyl.py
@@ -0,0 +1,27 @@
+from pi import get_digits
+
+
+class PiSeedWeylGenerator:
+
+    def __init__(self):
+        self.seed = 0
+        self.pi = get_digits()
+        self.pi_index = 0
+        self.update_seed()
+        self.x = 0
+        self.w = 0
+
+    def next(self):
+        self.x = self.x * self.x % 1000000
+        self.w += self.seed % 1000000
+        self.update_seed()
+        self.x += self.w % 1000000
+        self.x = int(str(self.x)[len(self.x) / 2:] + str(self.x)[:len(self.x) / 2])
+        random = ''
+        for i in range(6):
+            random += get_digits()[i + self.x]
+        return int(random)
+
+    def update_seed(self):
+        self.seed = [self.pi[i + self.pi_index] for i in range(6)]
+        self.pi_index += 6
