e03b34ead31e40e548f18523647b4ce187a4b415
m_ <61207790+MaksymIakovenko@users.noreply.github.com>
2022-05-24 11:04:35 +0200

    stirling memoisation

--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
+dico = {}
+
 def sterling(k, r):
 
     if r == 1 or k == r:
         return 1
 
-    res = sterling(k-1, r-1) + r * sterling(k-1, r)
+    global dico
+
+    res = dico.get((k, r))
+    if res == None: 
+        res = sterling(k-1, r-1) + r * sterling(k-1, r)
+        dico[(k, r)] = res
 
     return res
