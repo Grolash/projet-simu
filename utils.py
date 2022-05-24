f151557a57590f76c4063031b1d4b3db6b44c638
m_ <61207790+MaksymIakovenko@users.noreply.github.com>
2022-05-24 11:21:28 +0200

    Stirling Blanco

--- utils.py
+++ utils.py
@@ -1,15 +1,25 @@
+from math import factorial as f
+
 dico = {}
 
 def sterling(k, r):
 
-    if r == 1 or k == r:
-        return 1
+    # formula from: https://keisan.casio.com/exec/system/1292214964
+    return sum((-1)**(r-i)*(f(r)/(f(i)*f(r-i)))*i**k for i in range(r+1)) / f(r)
 
-    global dico
 
-    res = dico.get((k, r))
-    if res == None: 
-        res = sterling(k-1, r-1) + r * sterling(k-1, r)
-        dico[(k, r)] = res
+    # Reaches recursion limit
 
-    return res
+    # if r == 1 or k == r:
+    #     return 1
+
+    # global dico
+
+    # print(dico)
+
+    # res = dico.get((k, r))
+    # if res == None: 
+    #     res = sterling(k-1, r-1) + r * sterling(k-1, r)
+    #     dico[(k, r)] = res
+
+    # return res
